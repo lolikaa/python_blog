8801c2dad3f471e462f3e6d4808eeef73036e950
lolika <monik.gabrys@wp.pl>
2020-06-19 22:32:27 +0200

    front touchups

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,21 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from app.models import User
 from flask_login import current_user
+
+
+def validate_username(username):
+    user = User.query.filter_by(username=username.data).first()
+    if user:
+        raise ValidationError('Ta nazwa użytkownika jest już zajęta. Wybierz inną nazwę.')
+
+
+def validate_email(email):
+    user = User.query.filter_by(email=email.data).first()
+    if user:
+        raise ValidationError('Ten e-mail jest już zarejestrowany')
 
 
 class RegistrationForm(FlaskForm):
@@ -15,16 +28,6 @@ class RegistrationForm(FlaskForm):
     confirm_password = PasswordField('Potwierdź hasło',
                                      validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Zarejestruj się')
-
-    def validate_username(self, username):
-        user = User.query.filter_by(username=username.data).first()
-        if user:
-            raise ValidationError('Ta nazwa użytkownika jest już zajęta. Wybierz inną nazwę.')
-
-    def validate_email(self, email):
-        user = User.query.filter_by(email=email.data).first()
-        if user:
-            raise ValidationError('Ten e-mail jest już zarejestrowany')
 
 
 class LoginForm(FlaskForm):
@@ -42,22 +45,24 @@ class PostForm(FlaskForm):
     submit = SubmitField('Dodaj post')
 
 
+def validate_email(email):
+    if email.data != current_user.username:
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('Ten e-mail jest już zarejestrowany w bazie')
+
+
+def validate_username(username):
+    if username.data != current_user.username:
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('Ten login jest zajęty. Proszę wybierz inny login')
+
+
 class UpdateAccountForm(FlaskForm):
     username = StringField("Username",
                            validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Email',
                         validators=[DataRequired(), Email()])
+    picture = FileField('Wstaw nowe zdjęcie', validators=[FileAllowed(['jpg', 'png'])])
     submit = SubmitField('Zaktualizuj')
-
-    def validate_username(self, username):
-        if username.data != current_user.username:
-            user = User.query.filter_by(username=username.data).first()
-            if user:
-                raise ValidationError('Ten login jest zajęty. Proszę wybierz inny login')
-
-    def validate_email(self, email):
-        if email.data != current_user.username:
-            user = User.query.filter_by(email=email.data).first()
-            if user:
-                raise ValidationError('Ten e-mail jest już zarejestrowany w bazie')
-
--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,5 @@
+import os
+import secrets
 from os import abort
 
 from flask import redirect, url_for, render_template, flash, request
@@ -6,7 +8,7 @@ from flask_login import login_required, logout_user, login_user, current_user
 from app import app, db, bcrypt
 from app.forms import RegistrationForm, LoginForm, PostForm, UpdateAccountForm
 
-from app.models import User, Post, Category
+from app.models import User, Post
 
 
 @app.route("/")
@@ -155,13 +157,24 @@ def logout():
     return render_template('logout.html', tytul=tytul)
 
 
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+    form_picture.save(picture_path)
+    return picture_fn
+
+
 @app.route("/account", methods=['GET', 'POST'])
 @login_required
 def account():
     tytul = 'Twoje konto'
-    image_file = url_for('static', filename="profile_pics/" + current_user.image_file)
     form = UpdateAccountForm()
     if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
         current_user.username = form.username.data
         current_user.email = form.email.data
         db.session.commit()
@@ -170,4 +183,5 @@ def account():
     elif request.method == 'GET':
         form.username.data = current_user.username
         form.email.data = current_user.email
+    image_file = url_for('static', filename="profile_pics/" + current_user.image_file)
     return render_template('account.html', tytul=tytul, image_file=image_file, form=form)
